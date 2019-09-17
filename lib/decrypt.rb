63e03311a240d171a63858ebdf4c0471654de540
Jon Patterson <JonPatt92@gmail.com>
2019-09-17 17:14:03 -0600

    Build decrypt.rb CLI

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -12,3 +12,22 @@ require './lib/enigma'
 
 #$ ruby ./lib/decrypt.rb encrypted.txt decrypted.txt 82648 240818
 #Created 'decrypted.txt' with the key 82648 and date 240818
+
+message_path = ARGV[0]
+message_file = File.open(message_path, 'r')
+encrypted = message_file.readlines(chomp: true)
+encrypted_message = encrypted[0]
+encrypted_key = encrypted[1]
+encrypted_date = encrypted[2]
+
+
+enigma = Enigma.new
+decryption = enigma.decrypt(encrypted_message, encrypted_key, encrypted_date) # Could use ARGV[2] & ARGV[3] IF they are entered correctly.
+decrypted_message = decryption[:decryption]
+decrypted_file = ARGV[1]
+
+File.open("#{decrypted_file}", "w+") do |f|
+  f.write("#{decrypted_message}")
+end
+
+puts "Created '#{decrypted_file}' with the key #{decryption[:key]} and date #{decryption[:date]}"
